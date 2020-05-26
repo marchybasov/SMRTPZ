8d7a0fa42801b72fd4e84ea0e0b9ffdfaf9035df
maksym.archybasov <m.archibasov82@gmail.com>
2020-05-26 21:57:57 +0300

    Lab5 first commit

---
+++ Lab5/Lab5/FileReader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Transactions;
+
+namespace Lab5
+{
+    public class FileReader
+    {
+        private string _filePath;
+        public FileReader()
+        {
+           
+        }
+
+        public string[] ReadFile(string filePath)
+        {
+            _filePath = filePath;
+            return File.ReadAllLines(_filePath);
+        }
+    }
+}
+
+
